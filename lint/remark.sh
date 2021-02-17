8384510942973d9306676f4ddcb203355f746798
Owen Barton <owen@owenbarton.com>
2021-02-16 16:27:20 -0800

    Typo, prettify and drop eslint.

--- lint/remark.sh
+++ lint/remark.sh
@@ -2,12 +2,12 @@
 set -euo pipefail
 
 LOCAL_MASTER=$(git branch --list master)
-
 # Use local master if it exists, otherwise 
 MASTER=master
 if [[ -z ${LOCAL_MASTER} ]]; then
   MASTER=origin/master
 fi
+# Only check paths changed on branch to avoid excessive output.
 export REMARK_PATHS=$(git diff --diff-filter=AM --name-only "${MASTER}" | grep '^docs')
 
 if [[ -n "${REVIEWDOG_GITHUB_API_TOKEN:-}" ]]; then
@@ -16,16 +16,14 @@ if [[ -n "${REVIEWDOG_GITHUB_API_TOKEN:-}" ]]; then
       -name="remark-lint" \
       -fail-on-error="false" \
       -level="info" \
-      -tee \
-      ${REVIEWDOG_FLAGS}
+      -tee
 
   remark --rc-path=/usr/src/remarkrc.problem --no-color ${REMARK_PATHS} 2>&1 >/dev/null |
     reviewdog -f=remark-lint \
       -name="remark-lint" \
       -fail-on-error="true" \
       -level="error" \
-      -tee \
-      ${REVIEWDOG_FLAGS}
+      -tee
 else
   remark --quiet --rc-path=/usr/src/remarkrc.suggestion ${REMARK_PATHS} 2>&1 >/dev/null |
     sed -e 's/warning/suggestion/g'
