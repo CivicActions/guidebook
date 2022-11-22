f6adc00d3330c958b2805d107db9bed5b4fdbe6f
Daniel Mundra <dmundra@users.noreply.github.com>
2022-11-22 15:48:10 -0800

    Create extra.js

---
+++ docs/js/extra.js
@@ -0,0 +1,7 @@
+if (document.getElementById("rtd-search-form")) {
+  document.getElementById("rtd-search-form").firstElementChild.setAttribute("aria-label", "Search docs");
+}
+
+if (document.getElementById("content_search").getElementsByTagName("input")) {
+  document.getElementById("content_search").getElementsByTagName("input")[0].setAttribute("aria-label", "Search the Docs");
+}
