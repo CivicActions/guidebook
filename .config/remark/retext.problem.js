16b045180ae67ed1aa25cf54ec40e8829d7d9c9d
Daniel Mundra <daniel.mundra@civicactions.com>
2025-05-09 10:26:15 -0700

    Adjusted remark-gfm variable.

--- .config/remark/retext.problem.js
+++ .config/remark/retext.problem.js
@@ -9,19 +9,19 @@ import quotes from "retext-quotes";
 import sentenceSpacing from "retext-sentence-spacing";
 import repeated from "retext-repeated-words";
 import indefinite from "retext-indefinite-article";
-import gfm from "remark-gfm";
+import remarkGfm from "remark-gfm";
 
 export default attacher;
 
 function attacher() {
   this.use(
+    remarkGfm,
     remark2retext,
     unified()
       .use(english)
       .use(sentenceSpacing, { preferred: 1 })
       .use(quotes, { preferred: "straight" })
       .use(repeated)
-      .use(indefinite)
-      .use(gfm),
+      .use(indefinite),
   );
 }
